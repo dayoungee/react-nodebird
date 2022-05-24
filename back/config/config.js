783363c2151b0051e77ec7654ac6c53799a66f97
YunDayoung <dbsek21@naver.com>
2022-05-24 12:12:36 +0900

    db port modify

--- back/config/config.js
+++ back/config/config.js
@@ -8,7 +8,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: 'react-nodebird',
     host: '127.0.0.1',
-    port: '3305',
+    port: '3306',
     dialect: 'mysql',
   },
   test: {
