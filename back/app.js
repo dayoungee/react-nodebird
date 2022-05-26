fa47dcecce91c7a2bc436c26d859ef992c2cc7c5
YunDayoung <dbsek21@naver.com>
2022-05-26 12:18:45 +0900

    aws 배포용으로 변경

--- back/app.js
+++ back/app.js
@@ -5,6 +5,8 @@ const cookieParser = require('cookie-parser');
 const passport = require('passport');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const hpp = require('hpp');
+const helmet = require('helmet');
 
 const postRouter = require('./routes/post');
 const postsRouter = require('./routes/posts');
@@ -22,9 +24,17 @@ db.sequelize.sync()
   .catch(console.error);
 passportConfig();
 
-app.use(morgan('dev'));
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(morgan('combined'));
+  app.use(hpp());
+  app.use(helmet());
+}else{
+  app.use(morgan('dev'));
+}
+
 app.use(cors({
-  origin: 'http://localhost:3065',
+  origin: ['http://localhost:3065','nodebird.com'],
   credentials: true,
 }));
 app.use(express.json());
